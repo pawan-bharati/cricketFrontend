90f264a92e65955b7601ce0c70a1843b270610e0
pawan <pawanbharati90@gmail.com>
2020-02-04 23:41:30 +0545

    making of navigation bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
  import Register from './components/Register';
-// import PrivateRoute from './utils/PrivateRoute';
+ import PrivateRoute from './utils/PrivateRoute';
 // import Dashboard from './components/Dashboard';
 // import NoMatch from './components/NoMatch';
 // import UserProfile from './components/UserProfile';
