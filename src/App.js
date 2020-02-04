7e1483f0b6b04649d5456fd28b2c4c408d67e09d
pawan <pawanbharati90@gmail.com>
2020-02-04 22:46:05 +0545

    signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './Login';
-// import Register from './components/Register';
+import Login from './components/Login';
+ import Register from './components/Register';
 // import PrivateRoute from './utils/PrivateRoute';
 // import Dashboard from './components/Dashboard';
 // import NoMatch from './components/NoMatch';
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Login} />
-          {/* <Route path='/register' component={Register} /> */}
+          { <Route path='/register' component={Register} /> }
           {/* <PrivateRoute path='/dashboard' component={Dashboard} /> */}
           {/* <PrivateRoute path='/profile' component={UserProfile} /> */}
           
